49b81da75718272fb37c2a0d67a014dc372c4c5f
bartaadalbert <adalbertbarta@gmail.com>
2019-11-11 11:26:57 +0200

    new version off working app

--- app.py
+++ app.py
@@ -63,4 +63,4 @@ def register():
     return render_template('register.html', form=form)
 
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0', port=81)
+    app.run(debug=True, host='0.0.0.0', port=81)
--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/adalbertbarta/Documents/flaskApp/venv"
+set -gx VIRTUAL_ENV "/home/adalbert/git/flaskApp/flask/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
