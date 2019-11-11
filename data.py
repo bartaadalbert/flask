a6cae529e86f92c0dfb8c763c228d276e660a996
bartaadalbert <adalbertbarta@gmail.com>
2019-11-11 10:33:51 +0200

    flaskApp begins

---
+++ data.py
@@ -0,0 +1,28 @@
+def Articles():
+    articles = [
+        {
+            'id':1,
+            'title': 'Article One',
+            'body':'Something new',
+            'Author': 'Adalbert Barta',
+            'Date adding': '09.11.2019'
+
+        },
+        {
+            'id': 2,
+            'title': 'Article Two',
+            'body': 'Something new',
+            'Author': 'Nastia Barta',
+            'Date adding': '09.11.2019'
+
+        },
+        {
+            'id': 3,
+            'title': 'Article Three',
+            'body': 'Something new',
+            'Author': 'Marina Barta',
+            'Date adding': '09.11.2019'
+
+        }
+    ]
+    return articles
