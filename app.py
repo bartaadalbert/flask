d0f615a2f815e4b5912a65c67ab7d91735ed3949
bartaadalbert <adalbertbarta@gmail.com>
2019-11-12 08:00:01 +0200

    merge

--- app.py
+++ app.py
@@ -39,26 +39,7 @@ class RegisterForm(Form):
 def register():
     form = RegisterForm(request.form)
     if request.method == 'POST' and form.validate():
-        # name = form.name.data
-        # email = form.email.data
-        # username = form.username.data
-        # password = sha256_crypt.encrypt(str(form.password.data))
-        #
-        # # Create cursor
-        # cur = mysql.connection.cursor()
-        #
-        # # Execute query
-        # cur.execute("INSERT INTO users(name, email, username, password) VALUES(%s, %s, %s, %s)", (name, email, username, password))
-        #
-        # # Commit to DB
-        # mysql.connection.commit()
-        #
-        # # Close connection
-        # cur.close()
-        #
-        # flash('You are now registered and can log in', 'success')
-        #
-        # return redirect(url_for('login'))
+        
         return render_template('register.html')
     return render_template('register.html', form=form)
 
